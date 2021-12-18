9e8541191db45cac9bb74d3bc2d8746ed8a7c286
LeshVinov <90595946+LeshVinov@users.noreply.github.com>
2021-12-18 14:05:46 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,11 +1,49 @@
 const menuBtn = document.querySelector('.menu-btn');
 let menuOpen = false;
-menuBtn.addEventListener('click', () => {;
-    if (!menuOpen) {;
+
+const navBtn = document.querySelector('.navigation');
+let navOpen = false;
+
+const titleName = document.querySelector('.section_title');
+let titleNone = false;
+
+const flexEnd = document.querySelector('.name-and-burger');
+let endFlex = false;
+
+const descNone = document.querySelector('.desc_promo');
+let noneDesc = false;
+
+const rotationText = document.querySelector('.rotation_text-container')
+let textRotation = false;
+
+menuBtn.addEventListener('click', () => {
+    if (!menuOpen) {
         menuBtn.classList.add('open');
         menuOpen = true;
-    } else {;
-        menuBtn.classList.remove('open');
+        navBtn.classList.add('nav-open');
+        navOpen = true;
+        titleName.classList.add('nameNone');
+        titleNone = true;
+        flexEnd.classList.add ('flex_end');
+        endFlex = true;
+        descNone.classList.add ('desc_none');
+        noneDesc = true;
+        rotationText.classList.add ('text-appear');
+        textRotation = true;
+    } else {
+        menuBtn.classList.remove ('open');
         menuOpen = false;
+        navBtn.classList.remove ('nav-open');
+        navOpen = false;
+        titleName.classList.remove ('nameNone');
+        titleNone = false ;
+        flexEnd.classList.remove ('flex_end');
+        endFlex = false ;
+        descNone.classList.remove ('desc_none');
+        noneDesc = false;
+        rotationText.classList.remove ('text-appear');
+        textRotation = false;
     };
 });
+
+
